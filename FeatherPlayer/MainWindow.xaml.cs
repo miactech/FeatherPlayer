c67b9b16fa17dcd12b6593253171c34e80b9e063
adminTroll <1796785156@qq.com>
2019-02-15 12:58:46 +0800

    update

--- FeatherPlayer/MainWindow.xaml.cs
+++ FeatherPlayer/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,51 @@ namespace FeatherPlayer
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void wndMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            Blur.EnableBlur(this);
+        }
+
+        private void wndMain_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnExit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation daToRed = new DoubleAnimation() {
+                From = 0.0,
+                To = 1.0,
+                Duration = TimeSpan.FromMilliseconds(200)
+            };
+            btnExitBackground.BeginAnimation(Canvas.OpacityProperty, daToRed);
+        }
+
+        private void btnExit_MouseLeave(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation daToTrans = new DoubleAnimation()
+            {
+                From = 1.0,
+                To = 0.0,
+                Duration = TimeSpan.FromMilliseconds(200)
+            };
+            btnExitBackground.BeginAnimation(Canvas.OpacityProperty, daToTrans);
+        }
+
+        private void btnExit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            ///btnExitBackground.Background = Brushes.DarkRed;
+            Application.Current.Shutdown();
+        }
+
+        private void btnExit_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            btnExitBackground.Background = Brushes.OrangeRed;
+            Application.Current.Shutdown();
         }
     }
 }
