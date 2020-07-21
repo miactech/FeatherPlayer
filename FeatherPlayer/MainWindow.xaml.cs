34e9e7876f12f323b215d16e9aafffdcd462e80b
Micheal <Micheal@MICHEAL-COMPUTE>
2020-07-22 01:27:45 +0800

    2020年7月22日 01:26:59 Slider滑条美化/页面稍改动

--- FeatherPlayer/MainWindow.xaml.cs
+++ FeatherPlayer/MainWindow.xaml.cs
@@ -28,9 +28,9 @@ namespace FeatherPlayer
             string nextstr = "M10,16.5V7.5L16,12M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z";
             string pausestr = "M15,16H13V8H15M11,16H9V8H11M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z";
             nextdata = Geometry.Parse(nextstr);
-            pausedata = Geometry.Parse(pausestr);
-            VisibilityProperty = Visibility.Hidden;
+            pausedata = Geometry.Parse(pausestr);          
             InitializeComponent();
+            //playGrid.Visibility = Visibility.Hidden;
         }
         public enum playStatus
         {
