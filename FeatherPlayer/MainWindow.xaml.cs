bd6c42d2affa532729eb11403c67db68a068329e
adminTroll <1796785156@qq.com>
2020-07-26 22:05:25 +0800

    更改按钮调用方式

--- FeatherPlayer/MainWindow.xaml.cs
+++ FeatherPlayer/MainWindow.xaml.cs
@@ -59,12 +59,6 @@ namespace FeatherPlayer
             //MessageBox.Show("Test");
         }
 
-        private void wndMain_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            try { DragMove(); }
-            catch { }
-        }
-
         private void btnExit_MouseEnter(object sender, MouseEventArgs e)
         {
             DoubleAnimation daToRed = new DoubleAnimation() {
@@ -141,114 +135,6 @@ namespace FeatherPlayer
             btnMove.ScaleEasingAnimationShow(SongPic, 0.9, 1, 500);
         }
 
-        private void SongList1_MouseEnter(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList1, 0.9, 200);
-            btnMove.ScaleEasingAnimationShow(SongList1, 1, 0.9, 500);
-        }
-
-        private void SongList1_MouseLeave(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList1, 1, 200);
-            btnMove.ScaleEasingAnimationShow(SongList1, 0.9, 1, 500);
-        }
-
-        private void SongList2_MouseEnter(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList2, 0.9, 200);
-            btnMove.ScaleEasingAnimationShow(SongList2, 1, 0.9, 500);
-        }
-
-        private void SongList2_MouseLeave(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList2, 1, 200);
-            btnMove.ScaleEasingAnimationShow(SongList2, 0.9, 1, 500);
-        }
-
-        private void SongList3_MouseEnter(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList3, 0.9, 200);
-            btnMove.ScaleEasingAnimationShow(SongList3, 1, 0.9, 500);
-        }
-
-        private void SongList3_MouseLeave(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList3, 1, 200);
-            btnMove.ScaleEasingAnimationShow(SongList3, 0.9, 1, 500);
-        }
-
-        private void SongList4_MouseEnter(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList4, 0.9, 200);
-            btnMove.ScaleEasingAnimationShow(SongList4, 1, 0.9, 500);
-        }
-
-        private void SongList4_MouseLeave(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList4, 1, 200);
-            btnMove.ScaleEasingAnimationShow(SongList4, 0.9, 1, 500);
-        }
-
-        private void SongList5_MouseEnter(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList5, 0.9, 200);
-            btnMove.ScaleEasingAnimationShow(SongList5, 1, 0.9, 500);
-        }
-
-        private void SongList5_MouseLeave(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList5, 1, 200);
-            btnMove.ScaleEasingAnimationShow(SongList5, 0.9, 1, 500);
-        }
-
-        private void SongList6_MouseEnter(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList6, 0.9, 200);
-            btnMove.ScaleEasingAnimationShow(SongList6, 1, 0.9, 500);
-        }
-
-        private void SongList6_MouseLeave(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList6, 1, 200);
-            btnMove.ScaleEasingAnimationShow(SongList6, 0.9, 1, 500);
-        }
-
-        private void SongList7_MouseEnter(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList7, 0.9, 200);
-            btnMove.ScaleEasingAnimationShow(SongList7, 1, 0.9, 500);
-        }
-
-        private void SongList7_MouseLeave(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList7, 1, 200);
-            btnMove.ScaleEasingAnimationShow(SongList7, 0.9, 1, 500);
-        }
-
-        private void SongList8_MouseEnter(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList8, 0.9, 200);
-            btnMove.ScaleEasingAnimationShow(SongList8, 1, 0.9, 500);
-        }
-
-        private void SongList8_MouseLeave(object sender, MouseEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList8, 1, 200);
-            btnMove.ScaleEasingAnimationShow(SongList8, 0.9, 1, 500);
-        }
-
-        private void SongList1_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList1, 0.8, 200);
-            btnMove.ScaleEasingAnimationShow(SongList1, 0.9, 0.8, 500);
-        }
-
-        private void SongList1_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            btnOpacity.FloatElement(SongList1, 0.9, 200);
-            btnMove.ScaleEasingAnimationShow(SongList1, 0.8, 0.9, 500);
-        }
-
         private void PlayStop_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             string fileName;
@@ -289,6 +175,16 @@ namespace FeatherPlayer
                     player.Play();
                     break;
             }
+        }
+        /// <summary>
+        /// move window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void gridTitle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try { DragMove(); }
+            catch { }
         }
 
         private void Player_PlaybackStopped(object sender, PlaybackStoppedEventArgs e)
---
+++ FeatherPlayer/fButton.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FeatherPlayer
+{
+    /// <summary>
+    /// fButton.xaml 的交互逻辑
+    /// </summary>
+    public partial class fButton : UserControl
+    {
+        public fButton()
+        {
+            InitializeComponent();
+        }
+        private void b_MouseEnter(object sender, MouseEventArgs e)
+        {
+            OpacityAnimation(button, 0.9, 200);
+            ScaleEasingAnimationShow(button, 1, 0.96, 300);
+        }
+
+        private void b_MouseLeave(object sender, MouseEventArgs e)
+        {
+            OpacityAnimation(button, 1, 200);
+            ScaleEasingAnimationShow(button, 0.96, 1, 300);
+        }
+
+
+        private void b_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OpacityAnimation(button, 0.8, 100);
+            ScaleEasingAnimationShow(button, 0.96, 0.93, 150);//500
+        }
+
+        private void b_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            OpacityAnimation(button, 0.9, 100);
+            ScaleEasingAnimationShow(button, 0.93, 0.96, 150);
+        }
+
+        /// <summary>
+        /// 透明度动画
+        /// </summary>
+        /// <param name="elem">控件名</param>
+        /// <param name="to">目标透明度</param>
+        /// <param name="time">时间</param>
+        public static void OpacityAnimation(UIElement elem, double to, int time)
+        {
+            lock (elem)
+            {
+                if (to == 1)
+                {
+                    elem.Visibility = Visibility.Visible;
+                }
+                DoubleAnimation opacity = new DoubleAnimation()
+                {
+                    To = to,
+                    Duration = new TimeSpan(0, 0, 0, 0, time)
+                };
+                EventHandler handler = null;
+                opacity.Completed += handler = (s, e) =>
+                {
+                    opacity.Completed -= handler;
+                    if (to == 0)
+                    {
+                        elem.Visibility = Visibility.Collapsed;
+                    }
+                    opacity = null;
+                };
+
+                elem.BeginAnimation(UIElement.OpacityProperty, opacity);
+            }
+        }
+
+        /// <summary>
+        /// 缓动缩放动画
+        /// </summary>
+        /// <param name="element">控件名</param>
+        /// <param name="from">元素开始的大小</param>
+        /// <param name="to">元素到达的大小</param>
+        /// <param name="time">持续时间(毫秒)</param>
+        public static void ScaleEasingAnimationShow(UIElement element, double from, double to, int time)
+        {
+            lock (element)
+            {
+                ScaleTransform scale = new ScaleTransform();
+                element.RenderTransform = scale;
+                element.RenderTransformOrigin = new Point(0.5, 0.5);//定义圆心位置        
+                EasingFunctionBase easeFunction = new PowerEase()
+                {
+                    EasingMode = EasingMode.EaseOut,
+                    Power = 5
+                };
+                DoubleAnimation scaleAnimation = new DoubleAnimation()
+                {
+                    From = from,                                   //起始值
+                    To = to,                                     //结束值
+                    EasingFunction = easeFunction,                    //缓动函数
+                    Duration = new TimeSpan(0, 0, 0, 0, time)  //动画播放时间
+                };
+                AnimationClock clock = scaleAnimation.CreateClock();
+                scale.ApplyAnimationClock(ScaleTransform.ScaleXProperty, clock);
+                scale.ApplyAnimationClock(ScaleTransform.ScaleYProperty, clock);
+            }
+        }
+    }
+}
